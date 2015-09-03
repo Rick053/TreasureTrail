c32c00e5dd143f43563e523068e41886febd6c02
Rick053 <rvangemjr@gmail.com>
2015-09-03 12:14:12 +0200

    Added test data to populate the list and a handler to execute message checking every x seconds

--- app/src/main/java/com/utsdev/treasuretrail/MainActivity.java
+++ app/src/main/java/com/utsdev/treasuretrail/MainActivity.java
@@ -1,29 +1,71 @@
 package com.utsdev.treasuretrail;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Vibrator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-import tech.cocoon.Inbox.Inbox;
-import tech.cocoon.Monitor.Status;
-
+import com.utsdev.treasuretrail.adapters.HintAdapter;
+import com.utsdev.treasuretrail.stuff.DividerItemDecoration;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Vibrator v;
+    public RecyclerView rcv;
+    public HintAdapter adapter;
+
+    //test stuff
+    public int count = 1;
+
+    public String[] hints = new String[] {
+            "Test",
+            "Test 2",
+            "Hint",
+            "Lorem ipsum"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
         App.setActivity(this);
         App.startService(MainActivity.this);
 
-        TextView status = (TextView) App.getActivity().findViewById(R.id.status);
-        Status.assignView(status);
+//        TextView status = (TextView) App.getActivity().findViewById(R.id.status);
+//        Status.assignView(status);
 
+        //Initialize the adapter
+        adapter = new HintAdapter(this);
+
+        //Initialize the recyclerview which will display the hints
+        rcv = (RecyclerView) findViewById(R.id.hint_list);
+        rcv.setLayoutManager(new LinearLayoutManager(this));
+        rcv.setHasFixedSize(true);
+        rcv.setAdapter(adapter);
+        rcv.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
+
+        adapter.addHint(hints[1]);
+
+        //Initialize the handler which checks the messages every x seconds
+        final Handler handler = new Handler();
+        final int delay = 10000;
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                adapter.addHint(hints[count++]);
+
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
     }
 
 
--- app/src/main/java/com/utsdev/treasuretrail/adapters/HintAdapter.java
+++ app/src/main/java/com/utsdev/treasuretrail/adapters/HintAdapter.java
@@ -2,37 +2,54 @@ package com.utsdev.treasuretrail.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.utsdev.treasuretrail.R;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class HintAdapter extends RecyclerView.Adapter<HintAdapter.HintViewHolder> {
 
-    private List<String> data;
+    private List<String> data = new ArrayList<>();
     private Context context;
-    private View empty;
 
     public class HintViewHolder extends RecyclerView.ViewHolder {
 
+        TextView hint;
+
         public HintViewHolder(View view) {
             super(view);
+
+            hint = (TextView) view.findViewById(R.id.hint_text);
         }
     }
 
-    public HintAdapter(Context context, View emptyView) {
+    public HintAdapter(Context context) {
         this.context = context;
-        this.empty = emptyView;
     }
 
     @Override
     public HintViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        return null;
+        if ( viewGroup instanceof RecyclerView ) {
+            int layoutId = -1;
+            layoutId = R.layout.hint;
+            View view = LayoutInflater.from(viewGroup.getContext()).inflate(layoutId, viewGroup, false);
+            view.setFocusable(true);
+            return new HintViewHolder(view);
+        } else {
+            throw new RuntimeException("Not bound to RecyclerView");
+        }
     }
 
     @Override
     public void onBindViewHolder(HintViewHolder hintViewHolder, int i) {
+        String hint = data.get(i);
 
+        hintViewHolder.hint.setText(hint);
     }
 
     @Override
